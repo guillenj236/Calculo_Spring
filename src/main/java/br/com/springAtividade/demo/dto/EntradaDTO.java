0dc8986a3af21645439d3d2d181112663c9a3eeb
João Victor Guillen <guillenguilli@gmail.com>
2023-08-09 21:18:54 -0300

    first commit

---
+++ src/main/java/br/com/springAtividade/demo/dto/EntradaDTO.java
@@ -0,0 +1,14 @@
+package br.com.springAtividade.demo.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+public class EntradaDTO {
+    private List<Integer> numeros = new ArrayList<>();
+
+}
